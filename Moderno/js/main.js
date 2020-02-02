e584a4d866fe70874a0d9634213e0a0f09900faf
Vitaliy <ventik6@gmail.com>
2020-02-02 20:13:14 +0200

    d
    d

--- Moderno/js/main.js
+++ Moderno/js/main.js
@@ -11,6 +11,27 @@ $(function(){
         arrows: false,
         slidesToShow: 4,
         slidesToScroll: 4,
+        responsive: [
+          {
+            breakpoint: 1800,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 950,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          
+        ]
       });
 
       $(".js-range-slider").ionRangeSlider({
@@ -33,6 +54,27 @@ $(function(){
       $('.icon-th-large').addClass('active');
       $('.icon-th-list').removeClass('active');
     });
+
+    $('.product-one__tabs .tab, .settings__tabs .tab').on('click', function(event) {
+      var id = $(this).attr('data-id');
+        $('.product-one__tabs, .settings__tabs').find('.tab-item').removeClass('active-tab').hide();
+        $('.product-one__tabs .tabs, .settings__tabs .tab').find('.tab').removeClass('active');
+        $(this).addClass('active');
+        $('#'+id).addClass('active-tab').fadeIn();
+        return false;
+      });
+    
+$('input[type="file"], select').styler();
+
+$('.menu__btn').on('click', function(){
+  $('.menu__list').slideToggle();
+});
+
+$('.header__btn-menu').on('click', function(){
+  $('.header__box').toggleClass('active');
+});
+
+
     var mixer = mixitup('.products__inner-box');
     
 });
