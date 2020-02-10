3edc8358073681ccb7d93b23405b266377c16850
Vitaliy <ventik6@gmail.com>
2020-02-11 01:49:00 +0200

    up
    first str

---
+++ test/js/main.js
@@ -0,0 +1,13 @@
+$(function(){
+   
+
+    $('.team__inner-slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1 ,
+       
+        
+        
+      });
+});
