0c00fc8f45acf01da4dbe4dc1325eb3221fb484e
Vitaliy <ventik6@gmail.com>
2020-02-11 13:50:07 +0200

    up

--- test/js/main.js
+++ test/js/main.js
@@ -2,10 +2,15 @@ $(function(){
    
 
     $('.team__inner-slider').slick({
+        autoplay: true,
+        autoplaySpeed: 3000 ,
         dots: true,
         infinite: true,
-        speed: 300,
-        slidesToShow: 1 ,
+        speed: 600,
+        slidesToShow: 3,
+        slidesToScroll: 1 ,
+        centerMode: true ,
+        adaptiveHeight: true,
        
         
         
