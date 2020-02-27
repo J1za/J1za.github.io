18929e0a44b7b8804e01e97528d63e99eb559ba3
Vitaliy <ventik6@gmail.com>
2020-02-27 12:55:28 +0200

    first
    2 day

---
+++ outsours/js/main.js
@@ -0,0 +1,54 @@
+$(function(){
+
+    $('.slider').slick({
+        infinite: true,
+        speed: 1300,
+        
+        autoplaySpeed: 2100,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        adaptiveHeight: true,
+        dots: true
+      });
+  
+
+$('.accordion__title').click(function(){
+  $(this).closest('.accordion__item').siblings().removeClass('active').find('.accordion__content').slideUp(500);
+  $(this).closest('.accordion__item').toggleClass('active').find('.accordion__content').slideToggle(400);
+  return false;
+});
+
+
+
+
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let inputs = document.querySelectorAll('.input__file');
+    Array.prototype.forEach.call(inputs, function (input) {
+      let label = input.nextElementSibling,
+        labelVal = label.querySelector('.input__file-button-text').innerText;
+ 
+      input.addEventListener('change', function (e) {
+        let countFiles = '';
+        if (this.files && this.files.length >= 1)
+          countFiles = this.files.length;
+ 
+        if (countFiles)
+          label.querySelector('.input__file-button-text').innerText = 'Selected files: ' + countFiles;
+        else
+          label.querySelector('.input__file-button-text').innerText = labelVal;
+      });
+    });
