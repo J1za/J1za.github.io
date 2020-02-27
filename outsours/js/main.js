a688ae4ce66d83926d4f3c2173173665d7bc2e3e
Vitaliy <ventik6@gmail.com>
2020-02-27 21:35:44 +0200

    create
    site done

--- outsours/js/main.js
+++ outsours/js/main.js
@@ -11,14 +11,11 @@ $(function(){
         dots: true
       });
   
-
 $('.accordion__title').click(function(){
   $(this).closest('.accordion__item').siblings().removeClass('active').find('.accordion__content').slideUp(500);
   $(this).closest('.accordion__item').toggleClass('active').find('.accordion__content').slideToggle(400);
   return false;
 });
-
-
 
 
   });
