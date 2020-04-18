70701f57847a25f9b1f0907281f034b05002f500
j2za <ventik67@gmail.com>
2020-04-18 23:30:33 +0300

    up
    mobile

--- lk/js/main.js
+++ lk/js/main.js
@@ -4,6 +4,10 @@ $(document).ready(function () {
         $('.user__box,.user-profile').toggleClass('active');
       });
 
+      $('.header__burger').click(function(event){
+        $('.header__burger,.header__box').toggleClass('active');
+      });
+
       $("select").chosen({
         disable_search_threshold: 10
       })
