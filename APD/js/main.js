c93f0a1db49371bfb07d9a34d7a3a5693ece8092
j2za <ventik67@gmail.com>
2020-05-08 22:54:44 +0300

    up

--- APD/js/main.js
+++ APD/js/main.js
@@ -1,6 +1,30 @@
-$('.slick__slider').slick({
-    dots: true,
-    slidesToScroll: 1,
-    autoplay: false,
-    autoplaySpeed: 2000,
+
+
+$(document).ready(function () {
+
+    $('.slick__slider').slick({
+        dots: true,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [
+            {
+              breakpoint: 400,
+              settings: {
+                arrows: false,
+              }
+            },
+        ]
+    });
+    
+    $('.burger-menu').click(function(event){
+        $('.burger-menu, .menu').toggleClass('active');
+        $('body').toggleClass('lock');
+      });
+
+      
+
+     
+
+      
 });
