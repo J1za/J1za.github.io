f6be8b46598ffb292d751f520011e31da76ef52e
j2za <ventik67@gmail.com>
2020-07-07 19:31:50 +0300

    d
    d

--- wados/js/main.js
+++
@@ -1,7 +0,0 @@
-
-$('a[href*="#main"]').on('click', function(e){
-    $('html,body').animate({
-      scrollTop: $($(this).attr('href')).offset().top - 100
-    },1300);
-    e.preventDefault();
-  });
